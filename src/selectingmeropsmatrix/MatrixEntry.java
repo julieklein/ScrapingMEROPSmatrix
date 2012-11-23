48769d8763cce2498708924199af2ab29ae8bb18
Julie Klein <julie_klein64@yahoo.fr>
2012-11-23 10:16:16 +0000

    Memory eating code monster

---
+++ src/selectingmeropsmatrix/MatrixEntry.java
@@ -0,0 +1,161 @@
+package selectingmeropsmatrix;
+
+public class MatrixEntry {
+	public String proteasesymbol;
+	public String meropsurl;
+	public String nP4;
+	public String nP3;
+	public String nP2;
+	public String nP1;
+	public String nP1prime;
+	public String nP2prime;
+	public String nP3prime;
+	public String nP4prime;
+	public int dP4;
+	public int dP3;
+	public int dP2;
+	public int dP1;
+	public int dP1prime;
+	public int dP2prime;
+	public int dP3prime;
+	public int dP4prime;
+	public double Q75;
+	public double Q50;
+	public double Q25;
+	
+	public MatrixEntry() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getProteasesymbol() {
+		return proteasesymbol;
+	}
+	public void setProteasesymbol(String proteasesymbol) {
+		this.proteasesymbol = proteasesymbol;
+	}
+	
+	public String getnP2() {
+		return nP2;
+	}
+	public void setnP2(String nP2) {
+		this.nP2 = nP2;
+	}
+	
+	public double getQ75() {
+		return Q75;
+	}
+	public void setQ75(double q75) {
+		Q75 = q75;
+	}
+	public double getQ50() {
+		return Q50;
+	}
+	public void setQ50(double q50) {
+		Q50 = q50;
+	}
+	public double getQ25() {
+		return Q25;
+	}
+	public void setQ25(double q25) {
+		Q25 = q25;
+	}
+	public String getMeropsurl() {
+		return meropsurl;
+	}
+	public void setMeropsurl(String meropsurl) {
+		this.meropsurl = meropsurl;
+	}
+	public String getnP4() {
+		return nP4;
+	}
+	public void setnP4(String nP4) {
+		this.nP4 = nP4;
+	}
+	public String getnP3() {
+		return nP3;
+	}
+	public void setnP3(String nP3) {
+		this.nP3 = nP3;
+	}
+	public String getnP1() {
+		return nP1;
+	}
+	public void setnP1(String nP1) {
+		this.nP1 = nP1;
+	}
+	public String getnP1prime() {
+		return nP1prime;
+	}
+	public void setnP1prime(String nP1prime) {
+		this.nP1prime = nP1prime;
+	}
+	public String getnP2prime() {
+		return nP2prime;
+	}
+	public void setnP2prime(String nP2prime) {
+		this.nP2prime = nP2prime;
+	}
+	public String getnP3prime() {
+		return nP3prime;
+	}
+	public void setnP3prime(String nP3prime) {
+		this.nP3prime = nP3prime;
+	}
+	public String getnP4prime() {
+		return nP4prime;
+	}
+	public void setnP4prime(String nP4prime) {
+		this.nP4prime = nP4prime;
+	}
+	public int getdP4() {
+		return dP4;
+	}
+	public void setdP4(int dP4) {
+		this.dP4 = dP4;
+	}
+	public int getdP3() {
+		return dP3;
+	}
+	public void setdP3(int dP3) {
+		this.dP3 = dP3;
+	}
+	public int getdP2() {
+		return dP2;
+	}
+	public void setdP2(int dP2) {
+		this.dP2 = dP2;
+	}
+	public int getdP1() {
+		return dP1;
+	}
+	public void setdP1(int dP1) {
+		this.dP1 = dP1;
+	}
+	public int getdP1prime() {
+		return dP1prime;
+	}
+	public void setdP1prime(int dP1prime) {
+		this.dP1prime = dP1prime;
+	}
+	public int getdP2prime() {
+		return dP2prime;
+	}
+	public void setdP2prime(int dP2prime) {
+		this.dP2prime = dP2prime;
+	}
+	public int getdP3prime() {
+		return dP3prime;
+	}
+	public void setdP3prime(int dP3prime) {
+		this.dP3prime = dP3prime;
+	}
+	public int getdP4prime() {
+		return dP4prime;
+	}
+	public void setdP4prime(int dPprime) {
+		this.dP4prime = dPprime;
+	}
+	
+	
+	
+}
---
+++ src/selectingmeropsmatrix/ProbaForEachPosition.java
@@ -0,0 +1,26 @@
+package selectingmeropsmatrix;
+
+import java.util.ArrayList;
+
+public class ProbaForEachPosition {
+
+	private ArrayList<Integer> position;
+
+	
+	public ArrayList<Integer> getPosition() {
+		return position;
+	}
+
+
+	public void setPosition(ArrayList<Integer> position) {
+		this.position = position;
+	}
+
+
+	public ProbaForEachPosition() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+		
+	
+}
